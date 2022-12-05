c15d4f2cc7103ab35a1cf07eeba1eeb11013a872
Akilah Powell <atpjun@gmail.com>
2022-12-05 19:06:00 -0400

    created the event listerners in profile editor added avatar jpg to index.html and js for editor

--- profile-editor/profile-editor.js
+++ profile-editor/profile-editor.js
@@ -1,4 +1,4 @@
-import { getUser } from '../fetch-utils.js';
+import { getProfileById, getUser, uploadImage, upsertProfile } from '../fetch-utils.js';
 
 const errorDisplay = document.getElementById('error-display');
 const preview = document.getElementById('preview');
@@ -12,3 +12,74 @@ let error = null;
 let profile = null;
 
 const user = getUser();
+
+window.addEventListener('load', async () => {
+    const response = await getProfileById(user.id);
+    // console.log('response', response);
+    error = response.error;
+    profile = response.data;
+
+    if (error) {
+        errorDisplay.textContent = error.message;
+    } else {
+        if (profile) {
+            userNameInput.value = profile.username;
+            if (profile.avatar_url) {
+                preview.src = profile.avatar_url;
+            }
+            if (profile.bio) {
+                bioInput.value = profile.bio;
+            }
+        }
+    }
+});
+
+profileForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    updateButton.disabled = true;
+    updateButton.textContent = 'Saving...';
+
+    const formData = new FormData(profileForm);
+    // initial profile object
+    const profileObj = {
+        username: formData.get('username'),
+        bio: formData.get('bio'),
+    };
+
+    //get avatar file from form
+    const imageFile = formData.get('avatar');
+
+    if (imageFile.size) {
+        const imagePath = `${user.id}/${imageFile.name}`;
+
+        const url = await uploadImage(imagePath, imageFile);
+
+        profileObj.avatar_url = url;
+
+    }
+
+    const response = await upsertProfile(profileObj);
+
+    error = response.error;
+
+    if (error) {
+        errorDisplay.textContent = error.message;
+        updateButton.disabled = false;
+        updateButton.textContent = 'Update Profile';
+    } else {
+        location.assign('/');
+    }
+
+});
+
+avatarInput.addEventListener('change', () => {
+    const file = avatarInput.files[0];
+    // if a file has been selected, we want to see the preview
+    if (file) {
+        // creates a string containing a URL representing the file object
+        preview.src = URL.createObjectURL(file);
+    } else {
+        preview.src = '/assets/avatar.jpg';
+    }
+});
